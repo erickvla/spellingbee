5517430faa7aa33a8f0a284d16dc38ddfc75cf6a
erickvla <erickv.gonzalez@gmail.com>
2019-06-22 20:46:11 -0500

    added child names variables in branch-20190621

--- info.php
+++ info.php
@@ -3,6 +3,11 @@
 	$name = "Erick Vladimir";
 	$lastname = "Gonzalez Ortiz";
 
+    $child1 = "Emilio";
+    $child2 = "Diego";
+    $child3 = "Santiago";
+    $child4 = "Mauricio";
+
 	echo $name . " " . $lastname;
 
 	phpinfo();
